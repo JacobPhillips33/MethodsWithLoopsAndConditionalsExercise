37f83481e506eb6d4e583635c1d78f12e078332a
Jacob Phillips <jakephillips.wv@gmail.com>
2022-09-14 22:10:27 -0500

    initial commit

---
+++ MethodsWithLoopsAndConditionals/Methods.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsWithLoopsAndConditionals
+{
+    internal class Methods
+    {
+        public static void SeparateMethods()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("------------------------------Next Method----------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+        public static void AllNumbersToThousand()
+        {
+            for (int i = 1000; i > -1001; i--)
+            {                
+                Console.WriteLine(i);
+            }
+        }    
+        
+        public static void EveryThirdNumber()
+        {
+            for (int i = 3; i < 1000; i+=3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static void EqualNumbers()
+        {
+            Console.Write("Please enter first number: ");
+            var num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter second number: ");
+            var num2 = int.Parse(Console.ReadLine());
+
+            var answer = (num1 == num2) ? "These numbers are equal." : "These numbers are not equal.";
+
+            Console.WriteLine(answer);
+        }
+
+        public static void OddOrEven()
+        {
+            Console.WriteLine("Please enter a number:");
+            var userInput = int.Parse(Console.ReadLine());
+
+            var answer = (userInput % 2 == 0) ? $"{userInput} is an even number." : $"{userInput} is an odd number.";
+
+            Console.WriteLine(answer);
+        }
+
+        public static void PositiveOrNegative()
+        {
+            Console.WriteLine("Please enter a number:");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput > 0)
+            {
+                Console.WriteLine($"{userInput} is a positive number.");
+            }
+            else if(userInput < 0)
+            {
+                Console.WriteLine($"{userInput} is a negative number.");
+            }
+            else
+            {
+                Console.WriteLine($"{userInput} is neither a positive nor a negative number.");
+            }
+        }
+
+        public static void VotingAge()
+        {
+            Console.WriteLine("Please enter your age:");
+            var userAge = int.Parse(Console.ReadLine());
+
+            var answer = (userAge >= 18) ? "You ARE old enough to vote." : "You are NOT old enough to vote.";
+
+            Console.WriteLine(answer);
+        }
+
+        public static void InRange()
+        {
+            Console.WriteLine("Please enter a number:");
+            var userInput = int.Parse(Console.ReadLine());
+
+            var answer = (userInput <= 10 && userInput >= -10) ? $"{userInput} is WITHIN the range of -10 to 10." : $"{userInput} is OUTSIDE the range of -10 to 10.";
+
+            Console.WriteLine(answer);           
+        }
+
+        public static void MultiplicationTable()
+        {
+            Console.Write("Please enter a number: ");
+            var userInput = int.Parse(Console.ReadLine());
+
+            int[] multiplicationTable = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+
+            foreach(int num in multiplicationTable)
+            {
+                Console.WriteLine(userInput * num);
+            }
+        }
+    }
+}
---
+++ MethodsWithLoopsAndConditionals/Program.cs
@@ -0,0 +1,32 @@
+﻿using MethodsWithLoopsAndConditionals;
+
+Console.WriteLine("This method prints all numbers from 1000 through -1000.");
+Methods.AllNumbersToThousand();
+Methods.SeparateMethods();
+
+Console.WriteLine("This method prints all numbers from 3 to 999 by 3 each time.");
+Methods.EveryThirdNumber();
+Methods.SeparateMethods();
+
+Console.WriteLine("This method compares two numbers to determine if they are equal.");
+Methods.EqualNumbers();
+Methods.SeparateMethods();
+
+Console.WriteLine("This method determines whether a number is odd or even.");
+Methods.OddOrEven();
+Methods.SeparateMethods();
+
+Console.WriteLine("This method determines whether a number is positive or negative.");
+Methods.PositiveOrNegative();
+Methods.SeparateMethods();
+
+Console.WriteLine("This method determines whether you are old enough to vote.");
+Methods.VotingAge();
+Methods.SeparateMethods();
+
+Console.WriteLine("This method determines whether a number is within the range of -10 to 10.");
+Methods.InRange();
+Methods.SeparateMethods();
+
+Console.WriteLine("This method prints the multiplication table from 1 to 12 for the entered number.");
+Methods.MultiplicationTable();
